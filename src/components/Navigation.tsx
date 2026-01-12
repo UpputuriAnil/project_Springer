7ed3733c391bb99a471e8ef472bc86be46b9d641
UpputuriAnil <anilupputuri00@gmail.com>
2026-01-12 11:34:27 +0530

    fix: add React import to Navigation component

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import React from 'react';
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
 import { Home, BarChart2, Users, Settings, User, LogOut } from 'lucide-react';
